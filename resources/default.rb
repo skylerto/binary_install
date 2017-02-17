190f0661dad6fe22c1e8d8b945579ec732aece75
Artem Clement <ArtemC@Indellient.com>
2017-02-17 17:33:40 -0500

    changed temp dir to file_cache_path, added usern parameter to allow the user to set preferred owner, defaults to root, removed group (for now)

--- resources/default.rb
+++ resources/default.rb
@@ -6,22 +6,22 @@ property :destination, String
 property :destinations, Hash
 property :archive, String
 property :strip_directory, String
-
+property :usern, String
 
 action :install do
   log "#{service}, #{source}, #{destination}, #{destinations}, #{archive}"
-
-
+  temp_dir = Chef::Config[:file_cache_path]
+  
   if node[:platform_family].include? 'win'
     if destinations['windows']
       destination = destinations['windows']
     end
-    zip_file = "#{node['binary_install']['default_drive']['windows']}/Windows/TEMP/#{source}"
+    zip_file = "#{temp_dir}/#{source}"
     directory "#{destination}" do
       recursive true
       action :create
     end
-    cookbook_file "#{node['binary_install']['default_drive']['windows']}/Windows/TEMP/#{source}" do
+    cookbook_file "#{temp_dir}/#{source}" do
       source source
       action :create
     end
@@ -37,40 +37,45 @@ action :install do
     if destinations['linux']
       destination = destinations['linux']
     end
+	if usern == ""
+		usern = 'root'
+	end
     # => On Ubuntu/Most linux distros
     directory "#{destination}/#{service}" do
-      owner 'root'
-      group 'root'
+      owner usern
       mode '0755'
       recursive true
       action :create
     end
-    cookbook_file "/tmp/#{source}" do
+    cookbook_file "#{temp_dir}/#{source}" do
       source source
       mode '0755'
+	  owner usern
       action :create
     end
     if archive.include? 'tar.gz'
       if strip_directory.eql? 'true'
         execute 'untar strip' do
-          command "tar xzf /tmp/#{source} -C #{destination}/#{service} --strip-components=1"
+          command "tar xzf #{temp_dir}/#{source} -C #{destination}/#{service} --strip-components=1"
+		  user usern
           action :run
         end
       else
         execute 'untar no strip' do
-          command "tar xzf /tmp/#{source} -C #{destination}/#{service}"
+          command "tar xzf #{temp_dir}/#{source} -C #{destination}/#{service}"
+		  user usern
           action :run
         end
       end
     elsif archive.eql? 'zip'
       if strip_directory.eql? 'true'
         execute 'unzip strip' do
-          command "unzip /tmp/#{source} && mv /tmp/#{source.sub(/\.zip/, '')} #{destination}/#{service}"
+          command "unzip #{temp_dir}/#{source} && mv #{temp_dir}/#{source.sub(/\.zip/, '')} #{destination}/#{service}"
           action :run
         end
       else
         execute 'unzip no strip' do
-          command "unzip /tmp/#{source} -d #{destination}/#{service}"
+          command "unzip #{temp_dir}/#{source} -d #{destination}/#{service}"
           action :run
         end
       end
